c1552d4a38a5744991a95a62d533ba6ee9845a64
Guilherme Andrade <guilherme-andrade1110@hotmail.com>
2019-08-27 18:41:43 -0300

    Optimizando para Apk

--- BuscarCep/BuscarCep/Services/ViaCepService.cs
+++ BuscarCep/BuscarCep/Services/ViaCepService.cs
@@ -9,17 +9,17 @@ namespace BuscarCep.Services
     class ViaCepService
     {
         private static string UrlBase = "https://viacep.com.br/ws/";
+        private static HttpClient cliente = new HttpClient();
 
         public static Endereco BuscarEnderecoViaCEP(string cep)
         {
-            HttpClient cliente = new HttpClient();
+           
             var result = cliente.GetAsync($"{UrlBase}/{cep}/json").GetAwaiter().GetResult();
             var endereco = result.Content.ReadAsAsync<Endereco>().GetAwaiter().GetResult();
             return endereco;
         }
         public static List<Endereco> BuscarCEPViaEndereco(string Uf, string Cidade, string Logradouro)
         {
-            HttpClient cliente = new HttpClient();
             var result = cliente.GetAsync($"{UrlBase}/{Uf}/{Cidade}/{Logradouro}/json").GetAwaiter().GetResult();
             var endereco = result.Content.ReadAsAsync<List<Endereco>>().GetAwaiter().GetResult();
             return endereco;
